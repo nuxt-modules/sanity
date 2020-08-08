d1c4a76c769c1479bcd319f9f4c6839c4a53832d
Daniel Roe <daniel@concision.co.uk>
2020-08-08 20:37:40 +0100

    perf: ⚡️ improve options dx and decrease final bundle size

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import { bold } from 'chalk'
 import consola from 'consola'
 import defu from 'defu'
 
-import { Module } from '@nuxt/types'
+import type { Module } from '@nuxt/types'
+import type { SanityClient } from '@sanity/client'
 
-import { SanityClient } from '@sanity/client'
 import { SanityConfiguration, createClient } from './client'
 
-export interface ModuleOptions extends Partial<SanityConfiguration> {
+const isNuxtBuild = process.client || process.server
+
+export interface SanityModuleOptions extends Partial<SanityConfiguration> {
   /**
    * Use a micro-client that only supports making queries.
    * @default false
@@ -21,12 +23,12 @@ export interface ModuleOptions extends Partial<SanityConfiguration> {
    */
   imageHelper?: boolean
 }
-const DEFAULTS: ModuleOptions = {
+const DEFAULTS: SanityModuleOptions = {
   imageHelper: true
 }
 const CONFIG_KEY = 'sanity'
 
-function validateConfig ({ projectId, dataset }: ModuleOptions) {
+function validateConfig ({ projectId, dataset }: SanityModuleOptions) {
   if (!projectId) {
     consola.warn(
       `Make sure you specify a ${bold('projectId')} in your sanity config.`
@@ -42,19 +44,25 @@ function validateConfig ({ projectId, dataset }: ModuleOptions) {
   }
 }
 
-const nuxtModule: Module<ModuleOptions> = function (moduleOptions) {
+const nuxtModule: Module<SanityModuleOptions> = function (moduleOptions) {
+  if (isNuxtBuild) {
+    return
+  }
+
   let sanityConfig: Record<string, any> = {}
 
   try {
     // eslint-disable-next-line
-    const { projectId, dataset } = require(resolve(
-      this.options.rootDir,
-      './sanity.json'
-    )).api
-    sanityConfig = { projectId, dataset }
+    if (process.server) {
+      const fs = require('fs-extra')
+      const { projectId, dataset } = fs.readJSONSync(
+        resolve(this.options.rootDir, './sanity.json')
+      ).api
+      sanityConfig = { projectId, dataset }
+    }
   } catch {}
 
-  const options = defu<ModuleOptions>(
+  const options = defu<SanityModuleOptions>(
     this.options[CONFIG_KEY],
     moduleOptions,
     sanityConfig,
@@ -78,7 +86,10 @@ const nuxtModule: Module<ModuleOptions> = function (moduleOptions) {
       )} as it cannot be resolved in your project dependencies.
        Try running ${bold('yarn add @sanity/client')} or ${bold(
         'npm install @sanity/client'
-      )}.`
+      )}.
+       To disable this warning, set ${bold(
+         'sanity: { minimal: true }'
+       )} in your nuxt.config.js.`
     )
   }
 
@@ -111,14 +122,14 @@ const nuxtModule: Module<ModuleOptions> = function (moduleOptions) {
   this.options.build.transpile = this.options.build.transpile || []
   this.options.build.transpile.push(/^@nuxtjs[\\/]sanity/)
 }
-;(nuxtModule as any).meta = require('../package.json')
+;(nuxtModule as any).meta = { name: '@nuxtjs/sanity' }
 
 declare module '@nuxt/types' {
   interface NuxtConfig {
-    sanity: ModuleOptions
+    sanity: SanityModuleOptions
   } // Nuxt 2.14+
   interface Configuration {
-    sanity: ModuleOptions
+    sanity: SanityModuleOptions
   } // Nuxt 2.9 - 2.13
   interface NuxtAppOptions {
     $sanity: {
