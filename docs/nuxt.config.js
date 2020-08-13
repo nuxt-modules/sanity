e41dc924ff37aa2dc5123515ed59b891aad95fcc
Sébastien Chopin <seb@nuxtjs.com>
2020-08-13 11:57:34 +0200

    docs: add ackee analytics

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -4,4 +4,10 @@ export default theme({
   generate: {
     routes: ['/'],
   },
+  buildModules: ['nuxt-ackee'],
+  ackee: {
+    server: 'https://ackee.nuxtjs.com',
+    domainId: 'ce4748c0-4ca5-45b2-87d2-6fb06ea01b45',
+    detailed: true,
+  }
 })
