ae296e5be251950f1237e3290d261a54e0f6f7ca
Daniel Roe <daniel@roe.dev>
2020-10-23 20:48:00 +0100

    test: test groq polyfill as well

--- test/unit/helpers.test.ts
+++ test/unit/helpers.test.ts
@@ -1,20 +1,33 @@
-import { groq } from '../../src/helpers'
+const behaviour = () => {
+  const variable = 'description'
+  const { groq } = require('../../src/helpers')
+  const result = groq`
+    *[_type == 'article'] {
+      title,
+      ${variable}
+    }
+  `
+
+  expect(result).toBe(`
+    *[_type == 'article'] {
+      title,
+      description
+    }
+  `)
+}
 
 describe('groq', () => {
-  it('converts tagged literal into string', () => {
-    const variable = 'description'
-    const result = groq`
-      *[_type == 'article'] {
-        title,
-        ${variable}
-      }
-    `
+  it('converts tagged literal into string', behaviour)
+})
 
-    expect(result).toBe(`
-      *[_type == 'article'] {
-        title,
-        description
-      }
-    `)
+describe('groq with polyfill', () => {
+  const raw = String.raw
+  beforeAll(() => {
+    jest.resetModules()
+    delete (String as any).raw
   })
+  afterAll(() => {
+    String.raw = raw
+  })
+  it('converts tagged literal into string with polyfill', behaviour)
 })
