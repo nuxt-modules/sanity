c641ac2f7a964ad81eb80d4ca728e8170e596e11
Daniel Roe <daniel@roe.dev>
2021-05-15 14:43:07 +0100

    test: add blockquote snapshot

--- test/unit/sanity-content.test.ts
+++ test/unit/sanity-content.test.ts
@@ -137,6 +137,40 @@ const exampleBlocks: Record<string, ExampleBlock | ExampleBlock[]> = {
       listItem: 'bullet',
     },
   ],
+  blockquote: {
+    _key: 'd810da8ac845',
+    _type: 'block',
+    children: [
+      {
+        _key: 'd810da8ac8450',
+        _type: 'span',
+        marks: [],
+        text: 'Build your next Vue.js application with confidence using ',
+      },
+      {
+        _key: 'd810da8ac8451',
+        _type: 'span',
+        marks: [
+          'c9c6224401ba',
+        ],
+        text: 'NuxtJS',
+      },
+      {
+        _key: 'd810da8ac8452',
+        _type: 'span',
+        marks: [],
+        text: '. An open source framework making web development simple and powerful.',
+      },
+    ],
+    markDefs: [
+      {
+        _key: 'c9c6224401ba',
+        _type: 'link',
+        href: 'https://nuxtjs.org',
+      },
+    ],
+    style: 'blockquote',
+  },
   exampleMarkDefs: {
     _key: '3522a2a863b9',
     _type: 'block',
