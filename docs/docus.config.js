fd00e40022ac7ce31c62d24706d7b9549ec3fec2
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-21 01:14:44 +0200

    :wrench: (docus) add docus config; remove settings.json

---
+++ docs/docus.config.js
@@ -0,0 +1,17 @@
+export default {
+  title: '@nuxtjs/sanity',
+  url: 'https://sanity.nuxtjs.org',
+  theme: {
+    header: {
+      logo: {
+        dark: '/sanity-dark.svg',
+        light: '/sanity-light.svg',
+      },
+    },
+  },
+  github: {
+    repo: 'nuxt-community/sanity-module',
+    branch: 'main',
+    dir: 'docs',
+  },
+}
