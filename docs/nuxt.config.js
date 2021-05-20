0b7f60d9e9d1f9e2194393e7b99de06c81584060
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-21 01:13:41 +0200

    :wrench: (nuxt) update nuxt config

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,10 +1,6 @@
 import { withDocus } from 'docus'
 
 export default withDocus({
-  loading: { color: '#fa1607' },
-  generate: {
-    routes: ['/'],
-  },
   buildModules: ['vue-plausible'],
   ackee: {
     server: 'sanity.nuxtjs.org',
