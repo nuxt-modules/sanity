6a2242027d413b89386592ec038fc979c50a37f3
Daniel Roe <daniel@roe.dev>
2021-08-03 19:34:54 +0100

    style: fix `strong` indentation

--- test/unit/sanity-content.test.ts
+++ test/unit/sanity-content.test.ts
@@ -174,49 +174,48 @@ const exampleBlocks: Record<string, ExampleBlock | ExampleBlock[]> = {
     ],
     style: 'blockquote',
   },
-  strong:
-    {
-      _key: 'd810da8ac845',
-      _type: 'block',
-      children: [
-        {
-          _key: 'd810da8ac8450',
-          _type: 'span',
-          marks: [],
-          text: 'An example of ',
-        },
-        {
-          _key: '7dc51c030a2f',
-          _type: 'span',
-          marks: [
-            'strong',
-          ],
-          text: 'bold',
-        },
-        {
-          _key: '794714489c2d',
-          _type: 'span',
-          marks: [],
-          text: ' text and ',
-        },
-        {
-          _key: '23487f',
-          _type: 'span',
-          marks: [
-            'strong',
-          ],
-          text: 'another',
-        },
-        {
-          _key: '230498a',
-          _type: 'span',
-          marks: [],
-          text: '.',
-        },
-      ],
-      markDefs: [],
-      style: 'normal',
-    },
+  strong: {
+    _key: 'd810da8ac845',
+    _type: 'block',
+    children: [
+      {
+        _key: 'd810da8ac8450',
+        _type: 'span',
+        marks: [],
+        text: 'An example of ',
+      },
+      {
+        _key: '7dc51c030a2f',
+        _type: 'span',
+        marks: [
+          'strong',
+        ],
+        text: 'bold',
+      },
+      {
+        _key: '794714489c2d',
+        _type: 'span',
+        marks: [],
+        text: ' text and ',
+      },
+      {
+        _key: '23487f',
+        _type: 'span',
+        marks: [
+          'strong',
+        ],
+        text: 'another',
+      },
+      {
+        _key: '230498a',
+        _type: 'span',
+        marks: [],
+        text: '.',
+      },
+    ],
+    markDefs: [],
+    style: 'normal',
+  },
   exampleMarkDefs: {
     _key: '3522a2a863b9',
     _type: 'block',
