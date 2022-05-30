2d620d5f685beef550b4b4c6507fa039dcea5842
Florent <florentdelerue@hotmail.com>
2022-05-30 17:39:19 +0200

    refactor(docs): update to docus v2

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,35 @@
+import { defineNuxtConfig } from 'nuxt'
+import colors from 'tailwindcss/colors.js'
+
+export default defineNuxtConfig({
+  extends: ['./node_modules/@docus/docs-theme'],
+  github: {
+    repo: 'nuxtlabs/docus-starter',
+  },
+  tailwindcss: {
+    config: {
+      important: true,
+      theme: {
+        extend: {
+          colors: {
+            primary: {
+              50: '#FFF3F3',
+              100: '#FFE8E6',
+              200: '#FEC5C1',
+              300: '#FDA29C',
+              400: '#FC5C51',
+              500: '#FA1607',
+              600: '#E11406',
+              700: '#960D04',
+              800: '#710A03',
+              900: '#4B0702',
+            },
+          },
+        },
+      },
+    },
+  },
+  colorMode: {
+    preference: 'dark',
+  },
+})
