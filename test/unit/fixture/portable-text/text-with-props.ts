47c08ed356f2750a1baa0199f1dc90b60a858723
Andrew Grant <andrew@aadgrant.co.uk>
2022-10-12 17:56:40 +0100

    chore: added tests

---
+++ test/unit/fixture/portable-text/text-with-props.ts
@@ -0,0 +1,51 @@
+export const textWithProps = [
+  {
+    _key: 'd810da8ac842',
+    _type: 'block',
+    exampleProp: 'hello world',
+    children: [
+      {
+        _key: 'd810da8ac8450',
+        _type: 'span',
+        marks: [],
+        text: 'I’m a heading!',
+      },
+    ],
+    style: 'customStyle1',
+  },
+  {
+    _key: 'd810da8ac843',
+    _type: 'block',
+    class: 'blockquote',
+    children: [
+      {
+        _key: 'd810da8ac8450',
+        _type: 'span',
+        marks: [],
+        text: 'Build your next Vue.js application with confidence using ',
+      },
+      {
+        _key: 'd810da8ac8451',
+        _type: 'span',
+        marks: [
+          'c9c6224401ba',
+        ],
+        text: 'NuxtJS',
+      },
+      {
+        _key: 'd810da8ac8452',
+        _type: 'span',
+        marks: [],
+        text: '. An open source framework making web development simple and powerful.',
+      },
+    ],
+    markDefs: [
+      {
+        _key: 'c9c6224401ba',
+        _type: 'link',
+        href: 'https://nuxtjs.org',
+      },
+    ],
+    style: 'blockquote',
+  },
+]
