8830baa6a28d2cd8add0c4cf17851e892047030f
Daniel Roe <daniel@roe.dev>
2022-11-16 22:11:32 +0000

    chore: upgrade remaining dependencies (#565)

--- playground/nuxt.config.js
+++ playground/nuxt.config.js
@@ -4,8 +4,6 @@ export default defineNuxtConfig({
       script: [{ src: 'https://unpkg.com/tailwindcss-jit-cdn' }],
     },
   },
-  // remove in v3 - https://github.com/nuxt/framework/pull/9050
-  runtimeConfig: { public: {} },
   modules: ['@nuxtjs/sanity'],
   sanity: {
     globalHelper: true,
