39b74d28f198ea797da6e47550bcd1733f1fcb7c
Daniel Roe <daniel@roe.dev>
2022-11-16 11:44:34 +0000

    chore: migrate to rc14 and update docs

--- playground/nuxt.config.js
+++ playground/nuxt.config.js
@@ -4,6 +4,8 @@ export default defineNuxtConfig({
       script: [{ src: 'https://unpkg.com/tailwindcss-jit-cdn' }],
     },
   },
+  // remove in v3 - https://github.com/nuxt/framework/pull/9050
+  runtimeConfig: { public: {} },
   modules: ['@nuxtjs/sanity'],
   sanity: {
     globalHelper: true,
