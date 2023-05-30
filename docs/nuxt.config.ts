d2b8f57d4f6c4d5c61eb264999610164dd25a949
Daniel Roe <daniel@roe.dev>
2023-05-30 23:18:48 +0100

    chore: update nuxt version in docs + playground

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,5 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
-  // TODO: remove after v3.5 release
-  experimental: { inlineSSRStyles: false },
   modules: ['@nuxtjs/plausible'],
   plausible: {
     domain: 'sanity.nuxtjs.org',
