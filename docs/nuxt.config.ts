0c763fbc7bd92258f245695365e60b88ddb9bc19
Daniel Roe <daniel@roe.dev>
2023-05-30 23:03:00 +0100

    docs: add back disabled inline styles 🤔

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,5 +1,7 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
+  // TODO: remove after v3.5 release
+  experimental: { inlineSSRStyles: false },
   modules: ['@nuxtjs/plausible'],
   plausible: {
     domain: 'sanity.nuxtjs.org',
