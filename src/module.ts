e4a4ddb58934f22826e01964aff2ab225f4bafad
Daniel Roe <daniel@roe.dev>
2023-07-17 15:46:32 +0100

    fix: default sanity client to `raw` as well

--- src/module.ts
+++ src/module.ts
@@ -86,7 +86,7 @@ export default defineNuxtModule<SanityModuleOptions>({
       apiVersion: options.apiVersion,
       withCredentials: options.withCredentials,
       token: options.token,
-      perspective: options.perspective,
+      perspective: options.perspective || 'raw',
       additionalClients: options.additionalClients,
     })
 
