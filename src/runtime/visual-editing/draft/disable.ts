287b7033a4f0a766ee896cacb59516df97b37430
Daniel Roe <daniel@roe.dev>
2024-02-18 21:24:19 +0000

    fix: await redirect

--- src/runtime/visual-editing/draft/disable.ts
+++ src/runtime/visual-editing/draft/disable.ts
@@ -2,5 +2,5 @@ import { defineEventHandler, deleteCookie, sendRedirect } from 'h3'
 
 export default defineEventHandler(async event => {
   deleteCookie(event, '__sanity_draft')
-  sendRedirect(event, '/')
+  await sendRedirect(event, '/')
 })
