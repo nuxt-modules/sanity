fcbab9be98c2b9d28728e7a05cbaa0bb6d5c9c8e
Rupert Dunk <rupert@rupertdunk.com>
2024-03-12 07:52:49 -0700

    fix: respect redirect on preview disable

--- src/runtime/server/routes/preview/disable.ts
+++ src/runtime/server/routes/preview/disable.ts
@@ -1,6 +1,7 @@
-import { defineEventHandler, deleteCookie, sendRedirect } from 'h3'
+import { defineEventHandler, deleteCookie, getQuery, sendRedirect } from 'h3'
 
 export default defineEventHandler(async event => {
+  const { redirect } = getQuery(event)
   deleteCookie(event, '__sanity_preview')
-  await sendRedirect(event, '/')
+  await sendRedirect(event, redirect?.toString() || '/')
 })
