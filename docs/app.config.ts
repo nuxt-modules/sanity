a92bd9618960551aa6b7d45c8fa39a876b83e516
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2025-07-16 00:32:24 +0200

    set up docus 3 and copied content over

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,34 +1,5 @@
 export default defineAppConfig({
-  docus: {
+  header: {
     title: 'Nuxt Sanity',
-    layout: 'docs',
-    url: 'https://sanity.nuxtjs.org/',
-    description: 'Access text, images, and other media with Nuxt and the Sanity headless CMS.',
-    socials: { twitter: 'nuxt_js', github: 'nuxt-modules/sanity' },
-    debug: false,
-    // github: { root: 'docs/content', edit: true, releases: true },
-    cover: { src: '/cover.jpg', alt: 'A screenshot of the Nuxt Sanity module.' },
-    header: { title: false, logo: true },
-    footer: {
-      credits: [
-        {
-          icon: 'IconDocus',
-          text: 'Powered by Docus',
-          href: 'https://docus.com',
-        },
-      ],
-      icons: [
-        {
-          label: 'Nuxt',
-          href: 'https://nuxtjs.org',
-          component: 'IconNuxt',
-        },
-        {
-          label: 'Vue Telescope',
-          href: 'https://vuetelescope.com',
-          component: 'IconVueTelescope',
-        },
-      ],
-    },
   },
 })
