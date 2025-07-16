444af09d0b08adc47fa4efd63a9b21c4c28371fd
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2025-07-16 19:45:05 +0200

    Chore: added back SEO text and image

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -2,4 +2,11 @@ export default defineAppConfig({
   header: {
     title: 'Nuxt Sanity',
   },
+  seo: {
+    title: 'Nuxt Sanity',
+    description: 'Access text, images, and other media with Nuxt and the Sanity headless CMS.',
+    image: '/social-card.png',
+    siteName: 'Nuxt Sanity',
+    favicon: '/favicon.ico',
+  },
 })
