d29a5a7c730ad6444516f8dac2757fbab60295a6
Daniel Roe <daniel@roe.dev>
2025-07-21 17:37:48 +0200

    fix: add llms.domain

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/scripts'],
+  llms: {
+    domain: 'sanity.nuxtjs.org',
+  },
   scripts: {
     registry:
       {
