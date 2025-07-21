573a15c6e9e710b134fb840d9bab68fc8e49fb5b
Daniel Roe <daniel@roe.dev>
2025-07-21 18:00:11 +0200

    chore: fix docs build

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/scripts'],
+  site: {
+    name: 'Nuxt Sanity',
+  },
+  nitro: {
+    static: true,
+  },
   llms: {
     domain: 'sanity.nuxtjs.org',
   },
