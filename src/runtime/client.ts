4462d0d0c4d759bb46cc7701be9ed3d93a3d9539
Rupert Dunk <rupert@rupertdunk.com>
2026-02-11 16:05:18 +0000

    fix: resolve correct `useSanityQuery` fetch options

--- src/runtime/client.ts
+++ src/runtime/client.ts
@@ -2,4 +2,13 @@ import type { createClient } from '#sanity-client'
 
 export type SanityClient = ReturnType<typeof createClient>
 export { createClient } from '#sanity-client'
-export type { ClientConfig, ContentSourceMap, QueryParams, QueryOptions } from '#sanity-client'
+export type {
+  ClientPerspective,
+  ClientConfig,
+  ContentSourceMap,
+  InitializedClientConfig,
+  QueryParams,
+  QueryOptions,
+  StegaConfig,
+  UnfilteredResponseQueryOptions,
+} from '#sanity-client'
